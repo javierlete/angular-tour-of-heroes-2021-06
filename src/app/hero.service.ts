cfa8c42e6a9fc7e3924628b4507355b8ff03a863
Javier <jglete@eskura21.com>
2021-06-08 12:01:25 +0200

    Add Services: Show messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
+import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
@@ -8,7 +9,11 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
   }
 }
